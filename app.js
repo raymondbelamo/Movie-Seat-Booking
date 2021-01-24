812d62eaf957f76a92f456235474c300fc16bd4a
Raymond Belamo <raymondbelamo@yahoo.com>
2021-01-24 22:19:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("good to go!");
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const selectedSeatsCounts = selectedSeats.length;
+
+  count.innerText = selectedSeatsCounts;
+  total.innerText = selectedSeatsCounts * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener(change, e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+//Seat click event
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
+
